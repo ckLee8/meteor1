fcae13f09a5f0cc5b2c6ba4d866e92dfb61589b4
Christopher Lee <zerosxl@gmail.com>
2016-03-20 23:41:29 -0400

    add user permissions for posts

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,12 +1,25 @@
 Posts = new Mongo.Collection('posts');
 
+Posts.allow({
+  update: function(userId, post) { return ownsDocument(userId, post); },
+  remove: function(userId, post) { return ownsDocument(userId, post); },
+});
+
 Meteor.methods({
   postInsert: function(postAttributes) {
-    check(Meteor.userId(), String);
+    check(this.userId, String);
     check(postAttributes, {
       title: String,
       url: String
     });
+
+    var postWithSameLink = Posts.findOne({url: postAttributes.url});
+    if (postWithSameLink) {
+      return {
+        postExists: true,
+        _id: postWithSameLink._id
+      }
+    }
 
     var user = Meteor.user();
     var post = _.extend(postAttributes, {
